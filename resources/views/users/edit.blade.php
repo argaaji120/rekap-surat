9cbd1a0f040678fbdaf20704ba52610f1d9e7596
argaaji120 <argaaji120@gmail.com>
2020-02-05 22:25:35 +0700

    menambahkan tampilan edit user

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,127 @@
+@extends('layouts.app')
+
+@section('title', '| Edit Data User')
+
+@section('link')
+    <!-- bootstrap datepicker -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/select2/dist/css/select2.min.css') }}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ url('assets/plugins/iCheck/square/blue.css') }}">
+@endsection
+
+@section('content')
+    <section class="content-header">
+        <h1>User</h1>
+    </section>
+
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                      <h3 class="box-title">Edit Data User</h3>
+                    </div>
+
+                    <form action="{{route('users.update', [$user->id])}}" enctype="multipart/form-data" method="POST" role="form" autocomplete="off">
+                        @csrf
+                        <div class="box-body">
+                            <input type="hidden" value="PUT" name="_method">
+                            
+                            <!-- Username -->
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" name="username" class="form-control" id="username" placeholder="Username" value="{{ $user->username }}" readonly>
+                            </div>
+                            
+                            <!-- Nama -->
+                            <div class="form-group">
+                                <label for="nama">Nama</label>
+                                <input type="text" name="nama" class="form-control" id="nama" placeholder="Nama" value="{{ $user->name }}">
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="">Roles</label>
+                                <div class="radio">
+                                    <label>
+                                        <input {{ $user->roles == "Administrator" ? "checked" : "" }} type="radio" name="roles" value="Administrator" class="flat-blue">&nbsp; Administrator
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input {{ $user->roles == "Sekretaris" ? "checked" : "" }} type="radio" name="roles" value="Sekretaris" class="flat-blue">&nbsp; Sekretaris
+                                    </label>
+                                </div>
+                            </div>
+
+                            <!-- Foto -->
+                            <div class="form-group">
+                                <label for="avatar">Foto</label>
+                                @if ($user->avatar)
+                                    <img src="{{ asset('assets/dist/img/profile_avatar.jpg') }}" width="100px" class="img-responsive" alt="User Image">
+                                @else
+                                    <img class="img-responsive" src="" alt="avatar">
+                                @endif
+                                <input type="file" name="avatar" id="avatar">
+                                <p class="help-block">Example block-level help text here.</p>
+                            </div>
+
+                            <!-- Password -->
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+                            </div>
+
+                            <!-- Password Comfirmation -->
+                            <div class="form-group">
+                                <label for="password_confirmation">Password Confirmation</label>
+                                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="Password Confirmation">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="status">Status</label>
+                                <div class="radio">
+                                    <label>
+                                        <input {{ $user->status == "ACTIVE" ? "checked" : "" }} id="status" type="radio" name="status" value="Administrator" class="flat-blue">&nbsp; Active
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input {{ $user->status == "INACTIVE" ? "checked" : "" }} id="status" type="radio" name="status" value="Sekretaris" class="flat-blue">&nbsp; Inactive
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="box-footer">
+                            <button type="button" class="btn btn-flat btn-default" onclick="window.location.href='{{ route('users.index') }}'">
+                                <i class="fa fa-arrow-circle-left"></i>&nbsp; Kembali
+                            </button>
+                            <button type="submit" name="update" class="btn btn-flat btn-primary"><i class="fa fa-save"></i>&nbsp; Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('script')
+    <!-- bootstrap datepicker -->
+    <script src="{{ asset('assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+    <!-- Select2 -->
+    <script src="{{ asset('assets/plugins/select2/dist/js/select2.full.min.js') }}"></script>
+    <!-- iCheck -->
+    <script src="{{ url('assets/plugins/iCheck/icheck.min.js') }}"></script>
+@endsection
+
+@section('custom-script')
+   <script>
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' /* optional */
+        });
+   </script>
+@endsection
